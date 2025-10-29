0a9139f20fd86eb3ae2b971d2cb3a4babb47a8b3
Siddharth Das <66112421+eyepatch-3097@users.noreply.github.com>
2025-10-29 14:51:26 +0530

    Update settings.py

--- seocreator/settings.py
+++ seocreator/settings.py
@@ -83,28 +83,13 @@ WSGI_APPLICATION = 'seocreator.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-_db_url = os.getenv("DATABASE_URL")
-
-if _db_url:
-    # Internal Render DB usually does not need SSL.
-    # Allow overriding with DB_SSL_REQUIRE env var: "true"/"false"
-    _ssl_require_env = os.getenv("DB_SSL_REQUIRE", "false").lower().strip()
-    _ssl_require = _ssl_require_env in ("1", "true", "yes")
-
-    DATABASES = {
-        "default": dj_database_url.parse(
-            _db_url,
-            conn_max_age=600,         # persistent connections
-            ssl_require=_ssl_require  # false for INTERNAL URL
-        )
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL", f"sqlite:///{BASE_DIR/'db.sqlite3'}"),
+        conn_max_age=600,
+        ssl_require=False,
+    )
+}
    
 AUTH_USER_MODEL = "accounts.User"  # << custom user
 
