ca8409f97a5f539b0ef41d4680d9f71b57e5a250
Siddharth Das <66112421+eyepatch-3097@users.noreply.github.com>
2025-10-29 14:49:31 +0530

    Update ALLOWED_HOSTS to allow subdomains

--- seocreator/settings.py
+++ seocreator/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-!^rb6tf8*2q3@9%=(f23nqpo67fcjv6s60i!5g5&s84qfk_ux1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".onrender.com","dotswitch.space",".dotswitch.space","localhost","127.0.0.1"]
+ALLOWED_HOSTS = ["*.onrender.com","dotswitch.space","*.dotswitch.space","localhost","127.0.0.1"]
 CSRF_TRUSTED_ORIGINS = [
     *(f"https://{h}" for h in ALLOWED_HOSTS if h and h != "localhost" and h != "127.0.0.1")
 ]
